81d92bee3fb67e2f931a18ba10f019e04b08e070
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2021-12-19 19:15:23 +0530

    Add files via upload

---
+++ if Example.py
@@ -0,0 +1,25 @@
+#Largest Number:
+'''x = int(input("enter the x value"))
+y = int(input("enter the y value"))
+z = int(input("enter the z value"))
+
+if (x >= y) and (x >= z):
+    print("x is largest number")
+elif (y >= z) and (y >= x):
+    print("y is largest number")
+
+else:
+    print("z is largest number")'''
+
+
+
+x = 90
+y = 34
+z = 56
+if (x >= y) and (x >= z):
+    print("x is largest number")
+elif (y >= z) and (y >= x):
+    print("y is largest number")
+
+else:
+    print("z is largest number")
