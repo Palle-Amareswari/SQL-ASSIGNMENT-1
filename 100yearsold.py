1d6ef8f519c5dcdc82da9d743dafd48c389d918c
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2021-12-19 19:17:41 +0530

    Add files via upload

---
+++ 100yearsold.py
@@ -0,0 +1,13 @@
+'''name = input("enter your name:")
+age = int(input("enter your age:"))
+year = 2021 + (100 - age)
+
+print("manju", " You will be 100 years old in", year)'''
+
+
+#Using Function
+def fun(name, age):
+
+    age = 2021 + (100-age)
+    return (age)
+print(("loki",25))
